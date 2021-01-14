73bafdfcb1167168ca45fa821a559e30f6148522
Felix Roberge <froberge@cloudops.com>
2021-01-14 17:15:21 -0500

    fix typo

--- scripts/install_olm.sh
+++ scripts/install_olm.sh
@@ -1,3 +1,2 @@
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/0.15.1/crds.yaml
-
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/0.15.1/olm.yaml
